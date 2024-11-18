df0c9d7bcdfac4a2a0fd8c6c2b03f98b9c6fc178
ParkSeungWoo <qpfgud430@gmail.com>
2024-11-18 17:52:01 +0900

    feat: header 추가

--- apps/blog/src/app/layout.tsx
+++ apps/blog/src/app/layout.tsx
@@ -1,6 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
+import Header from "@/components/Header";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -29,6 +30,7 @@ export default function RootLayout({
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
         <main className="mx-auto mb-16 max-w-5xl px-5 py-24 sm:px-8">
+          <Header />
           {children}
         </main>
       </body>
