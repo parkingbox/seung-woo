5c10f67f963bef6675a02a215d20dd3a4ffe78ac
ParkSeungWoo <qpfgud430@gmail.com>
2024-11-20 11:43:57 +0900

    feat: edit header ui

--- apps/blog/src/components/Header.tsx
+++ apps/blog/src/components/Header.tsx
@@ -4,7 +4,7 @@ import Navbar from './Navbar'
 
 function Header() {
   return (
-    <header className='bg-background/30 fixed inset-x-0 top-4 z-40 mx-auto flex h-[60px] max-w-5xl items-center justify-between rounded-2xl px-8 shadow-sm saturate-100 backdrop-blur-[10px] transition-colors'>
+    <header className='bg-background/30 fixed inset-x-0 top-4 z-40 mx-auto flex h-[60px] max-w-5xl items-center justify-between rounded-2xl px-8 shadow-sm saturate-100 transition-colors'>
       <Link href='/' className='flex items-center justify-center gap-1'>
         <span className="text-[24px] font-bold italic tracking-[-0.3px] p-1 hover:underline active:bg-transparent">
           Seungwoo.
--- apps/blog/src/components/Navbar.tsx
+++ apps/blog/src/components/Navbar.tsx
@@ -9,28 +9,33 @@ function Navbar() {
   const pathname = usePathname()
 
   return (
-    <div className='flex'>
-      {HEADER_LINKS.map((item) => {
-        const isActive = item.href === pathname
-        return (
-          <li key={item.text} className='relative flex h-[60px] items-center justify-center'>
-            <Link
-              className={`rounded px-3 py-2 text-sm font-medium transition-colors ${isActive ? 'text-foreground' : 'text-muted-foreground hover:text-foreground'
-                }`}
-              href={item.href}
+    <nav>
+      <ul className="hidden gap-2 md:flex">
+        {HEADER_LINKS.map((link) => {
+          const isActive = link.href === pathname;
+
+          return (
+            <li
+              key={link.text}
+              className="relative flex h-[60px] items-center justify-center"
             >
-              {item.text}
-            </Link>
-            {isActive ? (
-              <>
-                <div className='bg-nav-link-indicator dark:bg-nav-link-indicator-dark absolute bottom-0 left-1/2 h-px w-12 -translate-x-1/2' />
-                <div className='absolute bottom-0 left-1/2 size-2.5 -translate-x-1/2 rounded-[4px] bg-[rgb(255_122_151)] blur dark:bg-[rgb(223_29_72)]' />
-              </>
-            ) : null}
-          </li>
-          )
-      })}
-    </div>
+              <Link
+                className={`rounded px-3 py-2 text-sm font-medium transition-colors`}
+                href={link.href}
+              >
+                {link.text}
+              </Link>
+              {isActive ? (
+                <>
+                  <div className="bg-nav-link-indicator dark:bg-nav-link-indicator-dark absolute bottom-0 left-1/2 h-px w-12 -translate-x-1/2" />
+                  <div className="absolute bottom-0 left-1/2 size-2.5 -translate-x-1/2 rounded-[4px] bg-[rgb(255_122_151)] blur dark:bg-[rgb(223_29_72)]" />
+                </>
+              ) : null}
+            </li>
+          );
+        })}
+      </ul>
+    </nav>
   )
 }
 
